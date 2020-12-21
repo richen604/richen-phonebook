666a70ea9fe4d42867f1fd8f3316dc2d0efc48e2
richen604 <56615615+richen604@users.noreply.github.com>
2020-12-20 21:27:57 -0800

    Exercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,19 +1,96 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-let notes = [
-  ...
-]
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId + 1;
+};
 
-const PORT = 3001
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  let person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date(Date.now());
+  response.send(
+    `<div>Phonebook info has ${
+      persons.length
+    } people</div> \n <div>${date.toString()}</div>`
+  );
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  person = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body) {
+    return response.status(400).end();
+  } else if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
